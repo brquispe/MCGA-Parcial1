ac2cbf1bff7e4b27943afba885f68422fa08c01f
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-10-01 18:22:05 -0300

    MCGA-04: Add get product by name or id endpoint

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -5,6 +5,24 @@ const getProductList = async (req, res) => {
   res.json(products);
 }
 
+const getProductByNameOrId = async (req, res) => {
+  const nameOrId = req.params.id;
+  const product = await Product.findOne({ $or: [
+    { name: nameOrId },
+    { id: nameOrId }
+  ] })
+
+  if (!product) {
+    return res.status(404).json({
+      error: true,
+      message: 'Product not found!'
+    })
+  }
+
+  res.json(product);
+}
+
 module.exports = {
-  getProductList
+  getProductList,
+  getProductByNameOrId
 };
--- src/routes/products.js
+++ src/routes/products.js
@@ -3,5 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/products', productController.getProductList);
+router.get('/products/:id', productController.getProductByNameOrId);
 
 module.exports = router;
